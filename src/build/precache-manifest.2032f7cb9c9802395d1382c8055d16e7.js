ca857665bbcbd3f21149ac74fc5da96ff14ed156
Yeo Su Jie <i170005@ntu.edu.sg>
2020-06-22 15:59:00 +0800

    First Commit

---
+++ src/build/precache-manifest.2032f7cb9c9802395d1382c8055d16e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea73bd41ee54f3c17b3dc7bddcaefa4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99cf9507204f1fa1a4c6",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "4d8e150510fe4f0f078d",
+    "url": "/static/js/2.a316455d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a316455d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99cf9507204f1fa1a4c6",
+    "url": "/static/js/main.e51a785e.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
